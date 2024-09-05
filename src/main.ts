dd61c49485c31928b77edafd4031996e5dc41643
seanmars <mars0222@gmail.com>
2024-09-05 11:35:22 +0800

    改用 vite+vue+primevue 建立網站，先將就網站移至 docs 底下

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import App from './App.vue'
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
+
